3e6d6207d3b718cbe8458058288069cb51f901be
Niyaz Khasanov <khasanchik85@gmail.com>
2024-07-01 21:15:08 +0500

    Создана рабочая ветка develop. Написаны докстринги к файлам Stack.py и Queue.py

--- _01_Stack/Stack.py
+++ _01_Stack/Stack.py
@@ -1,15 +1,24 @@
 class Node:
+    """Класс для создания вершины стэка"""
+
     def __init__(self, data, next_node=None):
+        """Функция инициализирует вершину стека. В data передаем данные, а next_node дает ссылку на следующую вершину
+    (по умолчанию None)"""
+
         self.data = data
         self.next_node = next_node
 
 
 class Stack:
+    """Класс для реализации стека"""
+
     def __init__(self, stack_size=5, top=None):
+        """Функция инициализирует стек. Размер стека (максимум 5) и вершина стека (по умолчанию None)"""
         self.stack_size = stack_size
         self.top = top  # через топ обращаемся к атрибутам ноды
 
     def push(self, data):
+        """Функция добавляет данные в стек. Если стек переполнен, возвращает соответствующее сообщение"""
         if self.size_stack() < self.stack_size:
             new_node = Node(data)
             new_node.next_node = self.top  # та вершина которая была
@@ -19,6 +28,7 @@ class Stack:
             return "Стэк переполнен"
 
     def pop(self):
+        """Функция удаляет последние добавленные данные. Если стек пуст, возвращает соответствующее сообщение"""
         if self.top:
             remove_last = self.top
             self.top = self.top.next_node
@@ -27,22 +37,27 @@ class Stack:
             return "Стэк пуст"
 
     def is_empty(self):
+        """Функция проверяет, является ли стек пустым. Возвращает True если пуст, иначе False"""
         if self.top:
             return False
         else:
             return True
 
     def is_full(self):
+        """Функция проверяет, является ли стек полным. Возвращает True если полон, иначе False"""
         if self.stack_size == self.size_stack():
             return True
         else:
             return False
 
     def clear_stack(self):
+        """Функция через цикл wile полностью удаляет данные из стека"""
         while self.top:
             self.pop()
 
     def get_data(self, index):
+        """Функция используя цикл wile возвращает данные из стека по его индексу. Если указанный индекс находится
+        вне диапозона возвращает сообение 'Out of range"""
         counter = 0
         stack_item = self.top
         while stack_item:
@@ -53,6 +68,7 @@ class Stack:
         return f"Out of range"
 
     def size_stack(self):
+        """Функция через счетчик и цикл wile определяет размер стека. Возвращает количество элементов стека"""
         counter = 0
         stack_item = self.top
         while stack_item:
@@ -61,6 +77,7 @@ class Stack:
         return counter
 
     def counter_int(self):
+        """Функция через счетчик и цикл wile определяет количество элементов int. Возвращает их количество"""
         counter = 0
         stack_item = self.top
         while stack_item:
@@ -77,3 +94,4 @@ stack.push(2)
 stack.push(2.5)
 stack.push("sta")
 print(stack.counter_int())
+
--- _02_Queue/Queue.py
+++ _02_Queue/Queue.py
@@ -1,15 +1,21 @@
 class Node:
+    """Класс, представляющий узел в очереди. Здесь data: данные, хранящиеся в узле,
+        next_node: ссылка на следующий узел в очереди."""
     def __init__(self, data, next_node=None):
+
         self.data = data
         self.next_node = next_node
 
 
 class Queue:
+    """Класс, представляющий структуру данных очередь.Здесь head: начало очереди,
+    tail: конец очереди."""
     def __init__(self, head=None, tail=None):
         self.head = head
         self.tail = tail
 
     def enqueue(self, data):
+        """Функция для добавления элемента в очередь, data: данные для добавления в очередь."""
         new_node = Node(data)
         if self.head is None:
             self.head = new_node
@@ -18,6 +24,7 @@ class Queue:
         self.tail = new_node
 
     def dequeue(self):
+        """Функция для извлечения элемента из очереди. Возвращает извлеченные данные из начала очереди."""
         if self.head is None:
             return None
         else:
